e51ed64d8bf1421b4a391ebe84a3f125f9fccc00
Lars Selstad <medlem@lase.no>
2024-11-11 22:50:29 +0100

    Split up build in chunks (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        visualizer({
+            filename: './dist/stats.html',
+            // change to true to automatically open the visualization in the default browser
+            open: false
+        })
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('react')) {
+                            return 'react';
+                        }
+                        if (id.includes('aws-amplify')) {
+                            return 'aws';
+                        }
+                        if (id.includes('lodash')) {
+                            return 'lodash';
+                        }
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    }
 });
