f5cbff6b1dc5ba4e82707422af23e19606032dd4
Lars Selstad <medlem@lase.no>
2025-02-22 21:00:38 +0100

    Added day name under daystatus

--- src/day/DayStatus.tsx
+++ src/day/DayStatus.tsx
@@ -4,10 +4,6 @@ import classNames from 'classnames';
 
 import './DayStatus.css';
 
-interface DayStatusProps {
-    day: DayObject;
-}
-
 const getEmoji = (day: DayObject): string => {
     if (!day.id) {
         return '';
@@ -19,16 +15,31 @@ const getEmoji = (day: DayObject): string => {
     return '';
 };
 
+// function to take Date as an input and return a string with 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør', 'Søn'
+const getDayName = (date: Date): string => {
+    const days = ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'];
+    return days[date.getDay()];
+};
+
+interface DayStatusProps {
+    day: DayObject;
+}
+
 const DayStatus: React.FC<DayStatusProps> = ({ day }) => {
+    console.log(day);
+
     return (
-        <li
-            className={classNames('daystatus', {
-                'daystatus-saved': day.id,
-                'daystatus-noChocolate-done': day.noChocolate,
-                'daystatus-stretching-done': day.stretching
-            })}
-        >
-            <span>{getEmoji(day)}</span>
+        <li>
+            <div
+                className={classNames('daystatus', {
+                    'daystatus-saved': day.id,
+                    'daystatus-noChocolate-done': day.noChocolate,
+                    'daystatus-stretching-done': day.stretching
+                })}
+            >
+                <span>{getEmoji(day)}</span>
+            </div>
+            <div className="textalign-center">{getDayName(day.date)}</div>
         </li>
     );
 };
