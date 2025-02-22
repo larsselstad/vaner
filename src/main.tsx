ae879600c9ad49d545fc3af1285c45affe2e85dc
Lars Selstad <medlem@lase.no>
2025-02-22 21:01:01 +0100

    Added hideSignUp to Authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
     ReactDOM.createRoot(rootElement).render(
         <React.StrictMode>
-            <Authenticator>
+            <Authenticator hideSignUp>
                 <App />
             </Authenticator>
         </React.StrictMode>
